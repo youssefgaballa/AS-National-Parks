f70a889f040f5da5c396c53dec55f6a11c189b76
youssefgaballa <yg01467@uga.edu>
2025-09-15 14:03:12 -0400

    added spinner

--- app/src/main/java/edu/uga/cs/nationalparks/MainActivity.java
+++ app/src/main/java/edu/uga/cs/nationalparks/MainActivity.java
@@ -1,6 +1,12 @@
 package edu.uga.cs.nationalparks;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.topics_array, android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
     }
+
+   
 }
